d5473a67a250e016fc6f47f19c8da74e10568355
akash <akashdinanathyadav321@gmail.com>
2024-05-02 17:36:21 +0530

    add kink of bg image

--- src/component/content/About.jsx
+++ src/component/content/About.jsx
@@ -4,7 +4,7 @@ import Links from './Link';
 function About() {
   return (<>
   <div className=" relative flex flex-col justify-center items-center h-screen text-black
-   w-full bg-[url('src/component/photos/brandi-redd-aJTiW00qqtI-unsplash.jpg')] bg-cover ">
+   w-full bg-[url('https://images.unsplash.com/photo-1470790376778-a9fbc86d70e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBvcnRmb2xpb3xlbnwwfHwwfHx8MA%3D%3D')] bg-cover ">
    
     <div className=' mt-28 h-screen p-4 text-xl w-full md:w-9/12'> <h2 className='text-2xl font-bold'> All About me:</h2>Welcome to my portfolio! I'm Akash Yadav, a passionate frontend developer with a keen eye for design and a love for crafting beautiful and user-friendly interfaces. With a strong foundation in HTML, CSS, JavaScript, Tailwind Css ,React and Photoshop I specialize in bringing creative ideas to life through clean, efficient, and responsive code.<br/><br />
   Driven by a desire to create seamless user experiences, I continuously strive to stay updated with the latest trends and technologies in the ever-evolving world of web development. From conceptualizing designs to implementing them with precision, I approach each project with enthusiasm and attention to detail.<br/><br />
